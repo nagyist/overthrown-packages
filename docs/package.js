21cadcc95c265b014a92d5f1b7f6c30930731a7d
Daniel Phillips <danphi@gmail.com>
2016-06-18 11:40:29 +0700

    initial

---
+++ docs/package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  name: 'danimal:overthrown-docs',
+  version: '0.0.1',
+  summary: 'Docs for OverThrown. http://overthrown.io',
+  git: 'https://github.com/dan335/overthrown-packages',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3.3.1');
+  api.use('ecmascript');
+  api.mainModule('docs.js');
+});
