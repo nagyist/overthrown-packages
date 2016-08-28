ce85705e8b10d7179bda2cb24a461bbf17ca94b6
Dan <danphi@gmail.com>
2016-08-28 19:42:37 -0400

    docs

--- docs/docs.js
+++ docs/docs.js
@@ -4,13 +4,15 @@ import './docsAlliances.html';
 import './docsBattles.html';
 import './docsMovement.html';
 import './docsTutorial.html';
+import './docsVillages.html';
 import './docsTurns.html';
 import './docsLinks.html';
+import './docsHiring.html';
 import './docsBlock.js';
 import './docs.less';
 
 Template.docsBase.helpers({
   getTemplate: function(templateName) {
-    return Blaze.toHTML(Template[templateName]);
+    return Blaze.toHTMLWithData(Template[templateName], {_s:_s});
   }
 })
